df0bd35af8af04aada6916937fed1c4fd8eec12b
Tanner <you@example.com>
2020-07-12 12:37:29 -0600

    ye

--- source/dialogbox/Dialogs.hx
+++ source/dialogbox/Dialogs.hx
@@ -3,7 +3,7 @@ package dialogbox;
 class Dialogs {
     var ArraytextList = ["Ugh its raining... and its cold...", "Gotta get some cigs"];
     public static var DialogArray:Array<Array<String>> = [
-        ["Move: WSAD/Arrow Keys", "Run: Shift", "Dive roll: Z/Space", "Objectives: Click book at bottom right", "Ugh its raining... and its cold...", "Gotta get some cigs."],
+        ["Move: WSAD/Arrow Keys", "Run: Shift", "Objectives: Click book at bottom right", "Dive roll: Z/Space - You can dive over cars", "Ugh its raining... and its cold...", "Gotta get some cigs."],
         ["Wait, I need a mask to shop here? Ok... I think Tony has one on the south side of town."],
         ["Great, a rainy mask.", "Feels like a wet diaper is on my face...", "Anything for a smoke I guess."],
         ["Ah! Finally some peace. Maybe Tillies has a good show tonight."],
